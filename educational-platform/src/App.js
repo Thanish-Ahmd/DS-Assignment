4f2bf8c009da0f2740ca207488ce9df3e5c55f88
Thanish-Ahmd <thanishahamed321@gmail.com>
2024-05-06 19:43:15 +0530

    routes

--- educational-platform/src/App.js
+++ educational-platform/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  BrowserRouter,
+} from "react-router-dom";
+import "./App.css";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+import LearnerDashboard from "./Components/LearnerDashboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/learnerDashboard" element={<LearnerDashboard />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
